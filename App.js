967904a012dce21af21190211ae02c3f94b079c0
Guilherme de Oliveira Machado <112173925+guilhermeomachadotech@users.noreply.github.com>
2024-09-17 09:24:29 -0300

    Primeiro Commit

--- App.js
+++ App.js
@@ -1,10 +1,56 @@
+import React from 'react-native';
+import { useEffect, useState } from 'react';
+import { CameraView, Camera } from "expo-camera";
+
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button} from 'react-native';
+import { Linking } from 'react-native';
 
 export default function App() {
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(()=>{
+    const getBarCodeScannerPermissions = async()=>{
+      const {status} = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({type, data}) =>{
+    setScanned(true);
+    Linking.canOpenURL(data).then(supported => {
+   if (supported) {
+      return Linking.openURL(data);
+   } else {
+      return Linking.openURL(data);
+   }
+});
+    
+  };
+
+  if (hasPermission === null) {
+      return <Text>Esperando a permissão de camera!</Text>
+  }
+
+  if (hasPermission === false) {
+      return <Text>A permissão à camera foi negada!</Text>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CameraView 
+          onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+          barcodeScannerSettings={{
+          
+        }}>
+      
+      </CameraView>
+      {scanned && <Button title={'Clique para escanear de novo'} onPress={()=> setScanned(false)}></Button>}
       <StatusBar style="auto" />
     </View>
   );
